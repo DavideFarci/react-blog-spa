a11fdb820296f6b6ff7d635d78d87f9cb2f7f58b
DavideFarci <yank92.df@gmail.com>
2023-12-11 16:11:01 +0100

    add router and routes, add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 // import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./Pages/DefaultLayout";
+import PostsList from "./components/PostsList";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>Ciao</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/posts" element={<PostsList />}></Route>
+          <Route path="/posts/:slug" element={<PostsList />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
